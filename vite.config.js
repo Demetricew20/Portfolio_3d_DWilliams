c1938b5d5bfc0dbcec3a38a799cd8dc829f2b03d
DemetriceW20 <demetrice_williams@hotmail.com>
2023-12-19 15:25:38 -0600

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Portfolio_3d_DWilliams.github.io',
+  base: '/Portfolio_3d_DWilliams.github.io',
   assetsInclude: ['**/*.glb']
 })
